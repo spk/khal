04436aec19821574dd0c4f9cec46bcc3ec31849b
Stephan Weller <stephan@weller.xyz>
2016-01-11 12:31:44 +0100

    One more whitespace change to make Linthub happy.

--- khal/controllers.py
+++ khal/controllers.py
@@ -96,7 +96,7 @@ def get_agenda(collection, locale, dates=None, firstweekday=0, days=None, events
             sys.exit(1)
 
     if week:
-        dates = [d - datetime.timedelta((d.weekday() - firstweekday)%7)
+        dates = [d - datetime.timedelta((d.weekday() - firstweekday) % 7)
                  for d in dates]
         days = 7
 
